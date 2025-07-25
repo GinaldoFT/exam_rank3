b10987eeacdd873e5cf00e5abb54ed98b647337f
GinaldoAORUS <gnaldinho79@gmail.com>
2025-07-25 19:56:06 +0100

    update

--- level-2/permutations/permutations.c
+++ level-2/permutations/permutations.c
@@ -1,20 +1,28 @@
 #include <unistd.h>
 
-void ft_swap(char *a, char *b)
+void	ft_swap(char *a, char *b)
 {
 	char tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
 	while (*str)
 		write(1, str++, 1);
 	write(1, "\n", 1);
 }
 
-void ft_sort(char *str, int len)
+int	ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_sort(char *str, int len)
 {
 	int i = 0;
 	while (i < len - 1)
@@ -30,35 +38,33 @@ void ft_sort(char *str, int len)
 	}
 }
 
-void permute(char *str, int start, int end)
+int	ft_next_permutation(char *str, int len)
 {
-	if (start == end)
-	{
-		ft_putstr(str);
-		return;
-	}
-	int i = start;
-	while (i <= end)
-	{
-		ft_swap(&str[start], &str[i]);
-		permute(str, start + 1, end);
-		ft_swap(&str[start], &str[i]);
-		i++;
-	}
+	int i = len - 2;
+	while (i >= 0 && str[i] >= str[i + 1])
+		i--;
+	if (i < 0)
+		return 0;
+	int j = len - 1;
+	while (str[j] <= str[i])
+		j--;
+	ft_swap(&str[i], &str[j]);
+	int start = i + 1;
+	int end = len - 1;
+	while (start < end)
+		ft_swap(&str[start++], &str[end--]);
+	return 1;
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc != 2)
 		return 1;
-
 	char *str = argv[1];
-	int len = 0;
-	while (str[len])
-		len++;
-
+	int len = ft_strlen(str);
 	ft_sort(str, len);
-	permute(str, 0, len - 1);
+	ft_putstr(str);
+	while (ft_next_permutation(str, len))
+		ft_putstr(str);
 	return 0;
 }
-
